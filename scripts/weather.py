2966c0b5a3eeacabf2509381cece672bb06278f2
Jonathan Barnard <jonathan.ebarnard955@gmail.com>
2020-08-27 18:02:43 +0100

    Removed bottom i3 bar

--- scripts/weather.py
+++ scripts/weather.py
@@ -3,9 +3,8 @@
 import requests
 import json
 
-with open('api.txt') as api_code:
+with open('/home/jonnybarns/.config/i3/scripts/api.txt') as api_code:
     api=api_code.read().splitlines()[0]
-
 URL = 'https://api.openweathermap.org/data/2.5/weather?id=3333123&units=metric&appid='+api
 
 GLYPHS = {
@@ -18,16 +17,17 @@ GLYPHS = {
     'atmosphere'    : '',
     }
 
-def save_data():
-    weather_data=requests.get(URL).json()
-    with open('data.txt', 'w') as file:
+def save_data(weather_data):
+    with open('scripts/data.txt', 'w') as file:
         json.dump(weather_data, file)
 
 def main():
     weather_data=requests.get(URL).json() 
     temperature = weather_data['main']['temp']
     current_condition = weather_data['weather'][0]['main'].lower()
- 
+
+    save_data(weather_data)
+
     for glyph, icon in GLYPHS.items():
         if glyph in current_condition:
             current_icon = icon
